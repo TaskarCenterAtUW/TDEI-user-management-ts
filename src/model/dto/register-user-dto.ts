60d6836e53836f2032ce92b45b0de46c836d8d6f
priya-badmanji <priyadarshini.badmanji@gmail.com>
2023-02-21 15:54:06 +0530

    password policy update

--- src/model/dto/register-user-dto.ts
+++ src/model/dto/register-user-dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty, Matches } from "class-validator";
 import { BaseDto } from "./base-dto";
 
 export class RegisterUserDto extends BaseDto {
@@ -9,6 +9,9 @@ export class RegisterUserDto extends BaseDto {
     email!: string;
     phone!: string;
     @IsNotEmpty()
+    @Matches('^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{8,}$', "", {
+        message: `Password policy not satisfied. >8 characters length, atleast 1 letter in Upper Case, atleast 1 Special Character (!@#$&*()), atleast 1 numeral (0-9)
+`})
     password!: string;
 
     constructor(init?: Partial<RegisterUserDto>) {
