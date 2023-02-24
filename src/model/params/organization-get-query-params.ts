0d0ef9d8b26cf014208fc80987267ccff8b88056
priya-badmanji <priyadarshini.badmanji@gmail.com>
2023-02-24 16:59:53 +0530

    bug fix org get

--- src/model/params/organization-get-query-params.ts
+++ src/model/params/organization-get-query-params.ts
@@ -42,23 +42,23 @@ export class OrgQueryParams extends AbstractDomainEntity {
         inner join keycloak.user_entity ue on ur.user_id = ue.id
         `.replace(/\n/g, ""));
         queryObject.buildPagination(this.page_no, this.page_size);
-        queryObject.buildOrder("name", SqlORder.ASC);
+        queryObject.buildOrder("o.name", SqlORder.ASC);
         //Add conditions
         if (this.searchText != undefined && this.searchText.length != 0) {
-            queryObject.condition(` name LIKE $${queryObject.paramCouter++} `, this.searchText + '%');
+            queryObject.condition(` o.name LIKE $${queryObject.paramCouter++} `, this.searchText + '%');
         }
         if (this.orgId != undefined && this.orgId.length != 0) {
-            queryObject.condition(` org_id = $${queryObject.paramCouter++} `, this.orgId);
+            queryObject.condition(` ur.org_id = $${queryObject.paramCouter++} `, this.orgId);
         }
         if (this.bbox && this.bbox.length > 0 && this.bbox.length == 4) {
-            queryObject.condition(`polygon && ST_MakeEnvelope($${queryObject.paramCouter++},$${queryObject.paramCouter++},$${queryObject.paramCouter++},$${queryObject.paramCouter++}, 4326)`,
+            queryObject.condition(` o.polygon && ST_MakeEnvelope($${queryObject.paramCouter++},$${queryObject.paramCouter++},$${queryObject.paramCouter++},$${queryObject.paramCouter++}, 4326)`,
                 this.bbox);
         }
         else if (this.bbox.length > 0 && this.bbox.length != 4) {
             console.debug("Skipping bbox filter as bounding box constraints not satisfied.");
         }
         //Always pull active organization
-        queryObject.condition(` is_active = $${queryObject.paramCouter++} `, true);
+        queryObject.condition(` o.is_active = $${queryObject.paramCouter++} `, true);
         queryObject.buildGroupRaw("group by o.org_id, o.name, o.phone, o.address, o.polygon, o.url, o.is_active, ue.email ");
 
         return queryObject;
