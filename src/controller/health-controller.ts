c73bc180b1dc8ed2558a47efba21dc7d5916da7b
MashB <priyadarshini.badmanji@gmail.com>
2023-06-07 15:02:15 +0530

    setup of unit testing

--- src/controller/health-controller.ts
+++ src/controller/health-controller.ts
@@ -15,7 +15,7 @@ class HealthController implements IController {
 
     public getping = async (request: Request, response: express.Response) => {
         // return loaded posts
-        response.send("I'm healthy !!");
+        response.status(200).send("I'm healthy !!");
     }
 }
 
