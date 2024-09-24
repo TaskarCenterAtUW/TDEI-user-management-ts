5bd820fcb8407f9057264659f2c5e11bb072edda
MashB <priyadarshini.badmanji@gmail.com>
2024-09-24 12:03:11 +0530

    bug fix 1262

--- src/app.ts
+++ src/app.ts
@@ -62,7 +62,8 @@ class App {
 
     private initializeMiddlewares() {
         this.app.use(helmet());
-        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.json({ limit: '10mb' }));
+        this.app.use(bodyParser.text({ limit: '10mb' }));
         this.app.use(cors());
     }
 
