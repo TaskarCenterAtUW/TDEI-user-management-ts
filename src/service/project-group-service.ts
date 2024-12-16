34b779640cf01579f7d408c2206d9da75297953d
MashB <priyadarshini.badmanji@gmail.com>
2024-12-16 16:36:32 +0530

    Deactive service/pg error message handling

--- src/service/project-group-service.ts
+++ src/service/project-group-service.ts
@@ -33,13 +33,29 @@ class ProjectGroupService implements IProjectGroupService {
 
     async createProjectGroup(projectgroup: ProjectGroupDto): Promise<String> {
 
+        const query = {
+            text: `Select is_active, name from project_group where name=$1 limit 1`,
+            values: [projectgroup.project_group_name],
+        }
+        var result = await dbClient.query(query);
+
+        if (result.rows.length > 0 && !result.rows[0].is_active) {
+            let message = `A project group by the name of "${projectgroup.project_group_name}" is a deactivated project group that already exists within the system. To proceed, either reactivate the project group, or choose a different name for a new project group.`;
+            throw new DuplicateException(message);
+        }
+        else if (result.rows.length > 0 && result.rows[0].is_active) {
+            let message = `A project group by the name of "${projectgroup.project_group_name}" already exists within the system. To proceed, choose a different name for a new project group.`;
+            throw new DuplicateException(message);
+        }
+
         return await dbClient.query(projectgroup.getInsertQuery())
             .then(res => {
                 return res.rows[0].project_group_id;
             })
             .catch(e => {
                 if (e instanceof UniqueKeyDbException) {
-                    throw new DuplicateException(projectgroup.project_group_name);
+                    let message = `A project group by the name of "${projectgroup.project_group_name}" already exists within the system. To proceed, choose a different name for a new project group.`;
+                    throw new DuplicateException(message);
                 }
                 throw e;
             });
@@ -72,7 +88,8 @@ class ProjectGroupService implements IProjectGroupService {
             })
             .catch(e => {
                 if (e instanceof UniqueKeyDbException) {
-                    throw new DuplicateException(projectgroup.project_group_name);
+                    let message = `A project group by the name of "${projectgroup.project_group_name}" already exists within the system. To proceed, choose a different name for a project group.`;
+                    throw new DuplicateException(message);
                 }
                 throw e;
             });
