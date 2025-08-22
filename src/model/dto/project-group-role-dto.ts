10cbcb8fbc75c89549162a4ae06a9e896ef04e88
MashB <mahesh_badmanji@yahoo.com>
2025-08-22 13:16:15 +0530

    get viewer details

--- src/model/dto/project-group-role-dto.ts
+++ src/model/dto/project-group-role-dto.ts
@@ -1,8 +1,10 @@
+import { DatasetViewerDto } from "./dataset-viewer-dto";
+
 export class ProjectGroupRoleDto {
     tdei_project_group_id!: string;
     project_group_name!: string;
     roles!: string[];
-
+    data_viewer_config!: DatasetViewerDto;
     constructor(init?: Partial<ProjectGroupRoleDto>) {
         Object.assign(this, init);
     }
